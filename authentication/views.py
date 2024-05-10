1b7ff1eada6fdcd3c0405501b683d8c996b653d0
ichouare <issam.chouaref1998@gmail.com>
2024-05-10 16:48:47 +0100

    reqrequirements.txt

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .forms import LoginForm, RegisterForm
 # Create your views here.
 
 @api_view(['POST', ])
 def sign_up(request) :
     if request.method == 'POST':
         print(request.data)
+        form = RegisterForm()
         return Response({"message": "Hello, world!"})
      
